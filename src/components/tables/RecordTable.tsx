bbb1053c49568800fc55f58ffa16962e77ee28db
toriojohn <toriojohn@gmail.com>
2025-03-26 01:17:04 +0800

    checkbox

--- src/components/tables/RecordTable.tsx
+++ src/components/tables/RecordTable.tsx
@@ -66,28 +66,7 @@ const FormSchema = z.object({
 type FormData = z.infer<typeof FormSchema>;
 
 export const columns = ( handleEdit: (student: Student) => void) : ColumnDef<Student>[] => [
-  {
-    id: "select",
-    header: ({ table }) => (
-      <Checkbox
-        checked={
-          table.getIsAllPageRowsSelected() ||
-          (table.getIsSomePageRowsSelected() && "indeterminate")
-        }
-        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
-        aria-label="Select all"
-      />
-    ),
-    cell: ({ row }) => (
-      <Checkbox
-        checked={row.getIsSelected()}
-        onCheckedChange={(value) => row.toggleSelected(!!value)}
-        aria-label="Select row"
-      />
-    ),
-    enableSorting: false,
-    enableHiding: false,
-  },
+  
   {
     accessorKey: "student_name",
     header: ({ column }) => {
